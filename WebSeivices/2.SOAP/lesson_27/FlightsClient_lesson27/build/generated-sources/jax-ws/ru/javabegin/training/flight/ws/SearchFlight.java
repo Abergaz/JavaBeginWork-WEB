17009deee58ae845d0deee57ba7a469e6426a766
Abergaz <zagreba@gmail.com>
2020-01-27 12:01:16 +0300

    ВЭБ сервисы в JAVA 2. SOAP 27. Handlers Flight_WS_lesson_27 Flight_WS_lesson_27_h FlightClient_lesson27 FlightClient_lesson27_h

---
+++ WebSeivices/2.SOAP/lesson_27/FlightsClient_lesson27/build/generated-sources/jax-ws/ru/javabegin/training/flight/ws/SearchFlight.java
@@ -0,0 +1,106 @@
+
+package ru.javabegin.training.flight.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for searchFlight complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="searchFlight">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="arg1" type="{http://ws.flight.training.javabegin.ru/}city" minOccurs="0"/>
+ *         &lt;element name="arg2" type="{http://ws.flight.training.javabegin.ru/}city" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "searchFlight", propOrder = {
+    "arg0",
+    "arg1",
+    "arg2"
+})
+public class SearchFlight {
+
+    protected long arg0;
+    protected City arg1;
+    protected City arg2;
+
+    /**
+     * Gets the value of the arg0 property.
+     * 
+     */
+    public long getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Sets the value of the arg0 property.
+     * 
+     */
+    public void setArg0(long value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Gets the value of the arg1 property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link City }
+     *     
+     */
+    public City getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Sets the value of the arg1 property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link City }
+     *     
+     */
+    public void setArg1(City value) {
+        this.arg1 = value;
+    }
+
+    /**
+     * Gets the value of the arg2 property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link City }
+     *     
+     */
+    public City getArg2() {
+        return arg2;
+    }
+
+    /**
+     * Sets the value of the arg2 property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link City }
+     *     
+     */
+    public void setArg2(City value) {
+        this.arg2 = value;
+    }
+
+}
---
+++ WebSeivices/2.SOAP/lesson_27/Flights_WS_lesson27/src/java/ru/javabegin/training/flight/ws/SearchWS.java
@@ -0,0 +1,67 @@
+package ru.javabegin.training.flight.ws;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import javax.jws.HandlerChain;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.MTOM;
+import javax.xml.ws.soap.SOAPBinding;
+import ru.javabegin.training.flight.interfaces.Buy;
+import ru.javabegin.training.flight.interfaces.Check;
+import ru.javabegin.training.flight.interfaces.Search;
+import ru.javabegin.training.flight.interfaces.impls.BuyImpl;
+import ru.javabegin.training.flight.interfaces.impls.CheckImpl;
+import ru.javabegin.training.flight.interfaces.impls.SearchImpl;
+import ru.javabegin.training.flight.objects.Flight;
+import ru.javabegin.training.flight.objects.Passenger;
+import ru.javabegin.training.flight.objects.Reservation;
+import ru.javabegin.training.flight.spr.objects.City;
+import ru.javabegin.training.flight.spr.objects.Place;
+import ru.javabegin.training.flight.utils.GMTCalendar;
+
+
+@MTOM
+@WebService(serviceName = "SearchWS")
+//@BindingType(value = SOAPBinding.SOAP12HTTP_MTOM_BINDING)
+//@HandlerChain(file = "SearchWS_handler.xml")
+@HandlerChain(file = "SearchWS_handler.xml")
+public class SearchWS implements Search, Buy, Check {
+
+    private SearchImpl searchImpl = new SearchImpl();
+    private BuyImpl buyImpl = new BuyImpl();
+    private CheckImpl checkImpl = new CheckImpl();
+
+    @Override
+    public ArrayList<Flight> searchFlight(long date, City cityFrom, City cityTo) {
+
+        ArrayList<Flight> list = new ArrayList<>();
+        Calendar c = GMTCalendar.getInstance();
+        c.setTimeInMillis(date);
+
+        list.addAll(searchImpl.searchFlight(date, cityFrom, cityTo));
+
+        return list;
+    }
+
+    @Override
+    public ArrayList<City> getAllCities() {
+        ArrayList<City> list = new ArrayList<>();
+        list.addAll(searchImpl.getAllCities());
+        return list;
+    }
+
+    @Override
+    public boolean buyTicket(Flight flight, Place place, Passenger passenger, String addInfo) {
+        boolean result = false;
+
+        result = buyImpl.buyTicket(flight, place, passenger, addInfo);
+
+        return result;
+    }
+
+    @Override
+    public Reservation checkReservationByCode(String code) {
+        return checkImpl.checkReservationByCode(code);
+    }
+}
